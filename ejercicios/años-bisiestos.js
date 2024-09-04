fceb404b808d6c26615175b89c7bf204f5c964b4
Fede Krenn <fedekrenn@gmail.com>
2024-09-03 21:47:37 -0300

    feat: Add function to print the next 30 leap years

---
+++ ejercicios/años-bisiestos.js
@@ -0,0 +1,13 @@
+/*
+* Crea una función que imprima los 30 próximos años bisiestos
+* siguientes a uno dado.
+* - Utiliza el menor número de líneas para resolver el ejercicio.
+*/
+
+function leapYears () {
+  for (let i = 2024; i < 2054; i = i + 4) {
+    console.log(i)
+  }
+}
+
+leapYears()
